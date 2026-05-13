64e269d6795acfa9ed0b4ca7e208ccc21dfff02f
WikyDev <wikinson72@gmail.com>
2026-05-12 20:36:29 -0500

    Actualización de aprendizaje HTML, incluida la carpeta de la página WikyTec

---
+++ 00-Basic/ExercicesStrings.js
@@ -0,0 +1,27 @@
+/*
+Clase 22 - Ejercicios: Strings
+Vídeo: https://youtu.be/1glVfFxj8a4?t=7226
+*/
+
+// 1. Concatena dos cadenas de texto
+console.log("Concatenar" + " texto" + ":)");
+// 2. Muestra la longitud de una cadena de texto
+let longitud = "Wilson"
+console.log(longitud.length)
+// 3. Muestra el primer y último carácter de un string
+console.log(longitud[0],longitud[5])
+// 4. Convierte a mayúsculas y minúsculas un string
+console.log(longitud.toUpperCase());
+console.log(longitud.toLowerCase());
+// 5. Crea una cadena de texto en varias líneas
+console.log("Cadena 1 \nCadena 2 \nCadena 3");
+// 6. Interpola el valor de una variable en un string
+const nombre = "Andres"
+console.log(`Hola, mi nombre es ${nombre}`)
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+// 9. Comprueba si dos strings son iguales
+
+// 10. Comprueba si dos strings tienen la misma longitud
